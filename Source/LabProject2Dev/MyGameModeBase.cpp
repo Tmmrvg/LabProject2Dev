1e9bd4704392626c8c0e873598586a11da02c698
Aleksthor <athorset98@gmail.com>
2023-03-22 22:01:28 +0100

    Circular Forests

---
+++ Source/LabProject2Dev/MyGameModeBase.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameModeBase.h"
+
+AMyGameModeBase::AMyGameModeBase()
+{
+
+}
+
+void AMyGameModeBase::BeginPlay()
+{
+}
+
+void AMyGameModeBase::Tick(float DeltaTime)
+{
+}
---
+++ Source/LabProject2Dev/MyGameModeBase.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LABPROJECT2DEV_API AMyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+public:
+	AMyGameModeBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+};
